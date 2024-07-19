d695acbc1d5b103a2bf4d37fd5305d100e3d0b3b
= <lunashih@byui.edu>
2024-07-19 10:29:24 -0600

    almost

--- app.py
+++ app.py
@@ -73,5 +73,8 @@ def flowchart():
     return render_template('flowchart.html')
 
 #---------------------------------------------------------------------------------------------------------------------
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
